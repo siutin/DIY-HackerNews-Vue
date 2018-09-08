710ea9d9e80bb54b599cb8c6acc5a3cf47149e5f
osiutino <osiutino@gmail.com>
2018-09-09 01:41:11 +0800

    extract card as row item

---
+++ src/components/RowItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card>
+    <v-progress-linear v-if="_.isEmpty(item)" :indeterminate="true"></v-progress-linear>
+
+    <v-card-title v-else primary-title>
+      <div class="headline">{{ index }} {{ id }} {{ maxScrolledPage }} {{ maxRowsPerPage }}</div>
+
+      </v-card-title>
+  </v-card>
+</template>
+
+<script>
+
+export default {
+  name: 'RowItem',
+  props: {
+    index: Number,
+    id: Number,
+    maxScrolledPage: Number,
+    maxRowsPerPage: Number
+  },
+  data () {
+    return {
+      item: {}
+    }
+  },
+  created () {
+    console.log(`RowItem - created`)
+  }
+}
+</script>
