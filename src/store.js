e7bdc43a42e0a34decbc7fb6b6a4272bd4306446
osiutino <osiutino@gmail.com>
2018-09-08 15:48:02 +0800

    keep new stores ID in vuex and list them all in home page

--- src/store.js
+++ src/store.js
@@ -3,14 +3,35 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
+const HACKER_NEWS_API_BASE_POINT = 'https://hacker-news.firebaseio.com/v0'
+const types = { HW_NEW_STORES: 'HW_NEW_STORES' }
 
-  },
-  mutations: {
+const state = {
+  newStoreIds: []
+}
 
-  },
-  actions: {
-
+const getters = {
+  getNewStoreIds: state => state.newStoreIds
+}
+const mutations = {
+  [types.HW_NEW_STORES] (state, payload) {
+    fetch(`${HACKER_NEWS_API_BASE_POINT}/newstories.json`)
+      .then(res => res.json())
+      .then(data => new Promise((resolve, reject) =>
+        Vue._.isArray(data) && !Vue._.isEmpty(data) ? resolve(data) : reject(new Error())
+      ))
+      .then(data => Vue.set(state, 'newStoreIds', data))
   }
+}
+const actions = {
+  syncHWNewStoreIDs ({ commit }) {
+    commit(types.HW_NEW_STORES)
+  }
+}
+
+export default new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,53 @@
 <template>
-  <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-      </v-layout>
-    </v-slide-y-transition>
-  </v-container>
+  <v-card>
+    <v-container fluid grid-list-lg>
+      <v-slide-y-transition mode="out-in">
+        <v-layout row wrap>
+
+         <v-flex xs12 v-for="id in getNewStoreIds">
+           <v-card color="blue-grey darken-2" class="white--text">
+             <v-card-title primary-title>
+               <div class="headline">{{ id }}</div>
+               </v-card-title>
+           </v-card>
+         </v-flex>
+
+        </v-layout>
+      </v-slide-y-transition>
+    </v-container>
+  </v-card>
 </template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  export default {
+    name: "Home",
+    data () {
+      return {}
+    },
+    computed: {
+      ...mapGetters([
+        'getNewStoreIds'
+      ])
+    },
+    methods: {
+      onRefreshClicked (){
+        console.log('onRefreshClicked')
+        this.updateData(this)
+      },
+      syncData (vm) {
+        vm.$store.dispatch('syncHWNewStoreIDs')
+      }
+    },
+    beforeRouteEnter (to, from , next) {
+      next(vm => vm.syncData(vm))
+    },
+    beforeRouteUpdate (to, from , next) {
+      next(vm => vm.syncData(vm))
+    }
+  }
+
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
