8bb553efd657bcbe1052195191c50ff917d50e90
osiutino <osiutino@gmail.com>
2018-09-08 16:49:53 +0800

    infinity scrolling List

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,16 @@
     <v-container fluid grid-list-lg>
       <v-slide-y-transition mode="out-in">
         <v-layout row wrap>
-
-         <v-flex xs12 v-for="(item, id) in getStores">
+         <v-flex xs12 v-for="(item, id) in getPaginatedList" :key="id">
            <v-card color="blue-grey darken-2" class="white--text">
              <v-card-title primary-title>
-               <div class="headline">{{ id }} {{ item }}</div>
+               <div class="headline">{{ id }} {{ item }} {{ maxScrolledPage }} {{ getMaxRowsPerPage }}</div>
                </v-card-title>
            </v-card>
          </v-flex>
-
+         <mugen-scroll :handler="updatePaginatedData" :should-handle="!loading">
+           loading...
+         </mugen-scroll>
         </v-layout>
       </v-slide-y-transition>
     </v-container>
@@ -19,16 +20,31 @@
 </template>
 
 <script>
+  import Vue from 'vue'
+  import MugenScroll from 'vue-mugen-scroll'
   import { mapGetters } from 'vuex'
+
+  const rowHeight = 88
   export default {
     name: 'Home',
+    components: { MugenScroll },
     data () {
-      return {}
+      return {
+        maxScrolledPage: 1,
+        loading: false
+      }
     },
     computed: {
       ...mapGetters([
         'getStores'
-      ])
+      ]),
+      getPaginatedList () {
+        let keys = Object.keys(this.getStores).sort().slice(0, this.getMaxRowsPerPage * this.maxScrolledPage)
+        return Vue._.pick(this.getStores, keys)
+      },
+      getMaxRowsPerPage () {
+        return Math.ceil((window.innerHeight - 56) / rowHeight)
+      }
     },
     methods: {
       onRefreshClicked () {
@@ -37,6 +53,11 @@
       },
       syncData (vm) {
         vm.$store.dispatch('syncHWNewStoreIDs')
+      },
+      updatePaginatedData () {
+        this.loading = true
+        this.maxScrolledPage++
+        this.loading = false
       }
     },
     beforeRouteEnter (to, from, next) {
