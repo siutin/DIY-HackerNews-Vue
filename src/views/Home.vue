e5807fe415ad7e0871944be57e08df3f54ddf0b1
osiutino <osiutino@gmail.com>
2018-09-10 01:25:17 +0800

    [FIX] screen flash issue when swiping

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card>
+  <v-card class="home">
     <v-container fluid grid-list-lg>
       <v-slide-y-transition mode="out-in">
         <v-layout row wrap align-center justify-center>
@@ -72,18 +72,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+
+.home {
+   overflow-x: hidden;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
