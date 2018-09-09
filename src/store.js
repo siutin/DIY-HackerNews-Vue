7db806d62348ccf8328ff2e62dc88de20eb30148
osiutino <osiutino@gmail.com>
2018-09-10 02:08:03 +0800

    eslint store.js

--- src/store.js
+++ src/store.js
@@ -22,18 +22,18 @@ const mutations = {
   [types.HW_NEW_STORES] (state, payload) {
     const newstories = window.localStorage.getItem(`${types.HW_NEW_STORES}`)
 
-    const last_updated_at = window.localStorage.getItem(`${types.HW_NEW_STORES}#last_updated_at`)
+    const lastUpdatedAt = window.localStorage.getItem(`${types.HW_NEW_STORES}#last_updated_at`)
     const useCache = moment().add(1, 'minutes').isAfter(moment(last_updated_at))
     console.log(`${types.HW_NEW_STORES} - useCache: ${useCache}`)
 
     if (newstories && useCache) {
       const data = JSON.parse(newstories)
       Vue._.each(data, storeId => {
-        if (state.storeIds.indexOf(storeId) == -1) {
+        if (state.storeIds.indexOf(storeId) === -1) {
           Vue.set(state, 'storeIds', [...state.storeIds, storeId])
         }
       })
-      if (typeof(payload) == 'function') {
+      if (typeof (payload) === 'function') {
         payload()
       }
     } else {
@@ -48,27 +48,26 @@ const mutations = {
           window.localStorage.setItem(`${types.HW_NEW_STORES}#last_updated_at`, moment().toISOString())
 
           Vue._.each(data, storeId => {
-            if (state.storeIds.indexOf(storeId) == -1) {
+            if (state.storeIds.indexOf(storeId) === -1) {
               Vue.set(state, 'storeIds', [...state.storeIds, storeId])
             }
           })
-          if (typeof(payload) == 'function') {
+          if (typeof (payload) === 'function') {
             payload()
           }
         }).catch(console.error)
     }
-
   },
   [types.HW_GET_STORE] (state, payload) {
     const { id, callback } = payload
     const store = window.localStorage.getItem(`store-${id}`)
 
-    const last_updated_at = window.localStorage.getItem(`store-${id}#last_updated_at`)
+    const lastUpdatedAt = window.localStorage.getItem(`store-${id}#last_updated_at`)
     const useCache = moment().add(1, 'minutes').isAfter(moment(last_updated_at))
     console.log(`${types.HW_GET_STORE} - store-${id} - useCache: ${useCache}`)
 
     if (store && useCache) {
-      if (typeof(callback) == 'function') {
+      if (typeof (callback) === 'function') {
         callback(JSON.parse(store))
       }
     } else {
@@ -76,14 +75,14 @@ const mutations = {
       fetch(`${HACKER_NEWS_API_BASE_POINT}/item/${id}.json`)
         .then(res => res.json())
         .then(data => new Promise((resolve, reject) =>
-         !Vue._.isEmpty(data) ? resolve(data) : reject(new Error('data is empty'))
+          !Vue._.isEmpty(data) ? resolve(data) : reject(new Error('data is empty'))
         ))
         .then(data => {
           Vue.set(state, 'stores', [...state.stores, data])
           window.localStorage.setItem(`store-${id}`, JSON.stringify(data))
           window.localStorage.setItem(`store-${id}#last_updated_at`, moment().toISOString())
 
-          if (typeof(callback) == 'function') {
+          if (typeof (callback) === 'function') {
             callback(data)
           }
         }).catch(err => console.error(err.message))
