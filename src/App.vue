6af5a13bb5709d9bbed33abd9fbbfb759ac02d54
siutin <osiutino@gmail.com>
2019-05-19 19:10:53 +0800

    change not to show story title in toolbar

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
 
      <v-spacer></v-spacer>
 
-      <v-toolbar-title>{{ getTitle || this.title }}</v-toolbar-title>
+      <v-toolbar-title>{{ this.title }}</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-spacer></v-spacer>
       <v-spacer></v-spacer>
